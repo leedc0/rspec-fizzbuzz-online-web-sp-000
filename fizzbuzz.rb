02713f26259f98cef370249ebbd464c4db9c9e07
Daniel Lee <leedc@me.com>
2019-06-29 16:15:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,30 +1,30 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-# def fizzbuzz(int)
-#   if int % 3 == 0 # if the number int is divisible by 3
-#     "Fizz" #Go fizz
-#   elsif int % 5 == 0 # if the number int is divisible by 5
-#     "Buzz" # Go buzz
-#   elsif int % 15 == 0 # if the number int is divisible by 3 or 5
-#     "FizzBuzz"
-#   else
-#     nil
-#   end
-# end
-#
-# fizzbuzz(3) # => You should see a return of "Fizz"
-# fizzbuzz(5) # => You should see a return of nil
-# fizzbuzz() # You should get an ArgumentError
-# fizzbuzz(15) # You should see a return of "FizzBuzz"
-
-def fizzbuzz (int)
-    if (int % 3 == 0 && int % 5 == 0)
-      fizz_15 = "FizzBuzz"
-    elsif int % 3 == 0
-      fizz_3 = "Fizz"
-    elsif int % 5 == 0
-      fizz_5 = "Buzz"
-    else
-      fizz_4 = nil
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" #Go fizz
+  elsif int % 5 == 0 # if the number int is divisible by 5
+    "Buzz" # Go buzz
+  elsif (int % 3 == 0 && int % 5 == 0) # if the number int is divisible by 3 or 5
+    "FizzBuzz"
+  else
+    nil
   end
 end
+
+fizzbuzz(3) # => You should see a return of "Fizz"
+fizzbuzz(5) # => You should see a return of nil
+fizzbuzz() # You should get an ArgumentError
+fizzbuzz(15) # You should see a return of "FizzBuzz"
+
+# def fizzbuzz (int)
+#     if (int % 3 == 0 && int % 5 == 0)
+#       fizz_15 = "FizzBuzz"
+#     elsif int % 3 == 0
+#       fizz_3 = "Fizz"
+#     elsif int % 5 == 0
+#       fizz_5 = "Buzz"
+#     else
+#       fizz_4 = nil
+#   end
+# end
